de57b10efb2e71c6da2f612e9de84524604e00d6
waku-miyamoto <67049564+waku-miyamoto@users.noreply.github.com>
2020-07-10 15:54:38 +0900

    devise-toppage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_08_032540) do
+ActiveRecord::Schema.define(version: 2020_07_10_064137) do
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "text", null: false
+    t.text "image"
+    t.text "movie"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
