923a7d4522d1e81378ca0d6a95566cd0333c4596
waku-miyamoto <67049564+waku-miyamoto@users.noreply.github.com>
2020-07-17 12:35:53 +0900

    user-showpage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.order('created_at DESC')
-    @posts = Post.includes(:user).where(user_id: current_user.id).page(params[:page]).per(10).order('updated_at DESC')
+    @posts = @user.posts.page(params[:page]).per(10).order('updated_at DESC')
   end
 end
