95922d1b2e2e069cbf27442a9e98d1193f3b3d4c
waku-miyamoto <67049564+waku-miyamoto@users.noreply.github.com>
2020-07-21 11:30:12 +0900

    alitle-edit

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,7 +5,11 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
+if Rails.env.production?
   storage :fog
+else
+  storage :file #=> ローカルストレージ
+end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -38,7 +42,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def extension_whitelist
   #   %w(jpg jpeg gif png)
   # end
-
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   # def filename
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,18 +1,21 @@
-equire 'carrierwave/storage/abstract'
+require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
-CarrierWave.configure do |config|
-  config.storage :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_directory  = 'lonerip-baket'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/lonerip-baket'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region: ENV['AWS_DEFAULT_REGION'],
-    path_style: true
-  }
-
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'  #=> 追加
+    config.fog_credentials = {
+      # Amazon S3用の設定
+      :provider              => 'AWS',
+      :region                => ENV['S3_REGION'],     # 例（東京）: 'ap-northeast-1'
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+    }
+    config.fog_directory     =  ENV['S3_BUCKET']
+    # キャッシュの保存期間
+    config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }
+  end
+  # 日本語ファイル名の設定
+  CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/ 
 end
