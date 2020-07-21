762e91dab38f0d66d6c480dd69ee961a07446a37
waku-miyamoto <67049564+waku-miyamoto@users.noreply.github.com>
2020-07-21 14:42:14 +0900

    comment-destroy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,11 @@ class CommentsController < ApplicationController
     @comment = Comment.create(comment_params)
     redirect_to post_path(params[:post_id])
   end
-
+  def destroy
+    Comment.find_by(id: params[:id],post_id: params[:post_id]).destroy
+    redirect_back(fallback_location: root_path)
+    
+  end
   private
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, post_id: params[:post_id])
